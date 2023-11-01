2c88d54cc03c1dafe7bdf12560355348ff8d3679
rpalrod0912 <rafapr0001@gmail.com>
2023-11-01 13:36:32 +0100

    feat: making prime ng and folder structure

---
+++ src/app/modules/components/auth/auth.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { AuthRoutingModule } from './auth-routing.module';
+import { SharedModule } from '../../shared/shared.module';
+
+@NgModule({
+  declarations: [LoginComponent, RegisterComponent],
+  exports: [LoginComponent, RegisterComponent],
+  imports: [CommonModule, AuthRoutingModule, SharedModule],
+})
+export class AuthModule {}
